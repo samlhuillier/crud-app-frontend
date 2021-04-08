ee4866adbd337ff8651d08e15f571feb80804f91
samlhuillier <sam.jansa123@gmail.com>
2021-04-08 14:03:14 +0100

    modification form can now show and hide

--- crud-app/src/app/components/user-item/user-item.component.ts
+++ crud-app/src/app/components/user-item/user-item.component.ts
@@ -10,10 +10,12 @@ export class UserItemComponent implements OnInit {
   @Output() deleteUser: EventEmitter<User> = new EventEmitter()
   @Output() modifyUser: EventEmitter<User> = new EventEmitter()
 
-  name: string;
-  email: string;
-
-  constructor() { }
+  name: string
+  email: string
+  wantsToModifyUserValues: boolean = true
+  constructor() { 
+    // this.wantsToModifyUserValues =false
+  }
 
   ngOnInit(): void {
   }
@@ -21,14 +23,20 @@ export class UserItemComponent implements OnInit {
   onDelete(user){
     this.deleteUser.emit(user)
   }
-
+  setClasses(){
+    return {
+      form: true,
+      hide: this.wantsToModifyUserValues
+    }
+  }
   onSubmit(){
     let user = {id: this.user.id, name: this.name, email: this.email}
     this.modifyUser.emit(user)
   }
-  // onModify(user){
-  //   user.name = user.name + 'MODIFICATION'
-  //   this.modifyUser.emit(user)
-  // }
+
+  wantModify(user){
+    this.wantsToModifyUserValues = !this.wantsToModifyUserValues
+  }
+  
 
 }
